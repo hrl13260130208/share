046d3e0c21691c826f0a38e92f4bd0234ffa630a
hrl <2046391563@qq.com>
2019-09-10 15:36:42 +0800

    hrl 2019-09-10

--- share/data_format.py
+++ share/data_format.py
@@ -2,6 +2,7 @@ import tushare as ts
 import numpy as np
 import redis
 import json
+import datetime
 
 
 
@@ -90,12 +91,23 @@ def save_line(ts_code,line_name,line_data):
 def create_name(ts_code,line_name,option_name):
     return str(ts_code)+"_"+str(line_name)+"_"+str(option_name)
 
-def normalization_line_data(ts_code,line_name,line_data,normalization_type=Max_Min_Normalization):
+def normalization_line_data(ts_code,line_name,line_data,normalization_type=Max_Min_Normalization,one_data=True):
+    '''
+    对指定列进行标准化处理
+    :param ts_code:
+    :param line_name:
+    :param line_data:
+    :param normalization_type:
+    :param one_data: True表示该列只有一个数据
+    :return:
+    '''
     if normalization_type==Max_Min_Normalization:
         max=float(redis_.get(create_name(ts_code,line_name,MAX_NAME)))
         min=float(redis_.get(create_name(ts_code,line_name,MIN_NAME)))
-        print(max,min)
-        return MaxMinNormalization(line_data,np.full(len(line_data),max),np.full(len(line_data),min))
+        if one_data:
+            return MaxMinNormalization(line_data,max,min)
+        else:
+            return MaxMinNormalization(line_data,np.full(len(line_data),max),np.full(len(line_data),min))
     elif normalization_type==Z_Score_Normalization:
         mu = float(redis_.get(create_name(ts_code, line_name, MU_NAME)))
         sigma =float(redis_.get(create_name(ts_code, line_name, SIGMA_NAME)))
@@ -157,26 +169,98 @@ class Data_Format():
         save_line(ts_code, VOL_LINE_NAME, np_data[:, 9])
         save_line(ts_code, AMONT_LINE_NAME, np_data[:, 10])
         for line in np_data:
-            print(line)
+            self.save_day_data(ts_code,line[1],line)
+
 
     def save_day_data(self,ts_code,day,data):
-        pass
+        mmstd_data=self.get_mmstad_data(ts_code,data)
+        map_name=self.create_ts_map_name(ts_code)
+        print(day)
+        redis_.hset(map_name,day,json.dumps({"source_data":data.tolist(),"mmstd_data":mmstd_data}))
 
 
+    def create_ts_map_name(self,ts_code):
+        return ts_code+"_days"
 
+    def get_mmstad_data(self,ts_code,data):
+        data1 = normalization_line_data(ts_code, OPEN_LINE_NAME, data[ 2])
+        data2 = normalization_line_data(ts_code, HIGH_LINE_NAME, data[ 3])
+        data3 = normalization_line_data(ts_code, LOW_LINE_NAME, data[ 4])
+        data4 = normalization_line_data(ts_code, CLOSE_LINE_NAME, data[5])
+        data5 = normalization_line_data(ts_code, VOL_LINE_NAME, data[9])
+        data6 = normalization_line_data(ts_code, AMONT_LINE_NAME, data[ 10])
+        return [ data1, data2, data3, data4, data5, data6]
 
+    def get_train_and_result_data(self,date):
+        '''
+        获取训练数据与对应结果
+            训练数据：七天内的数据
+            训练结果：0或1     1第二天的收盘价上涨，0下跌
+        :param date:
+        :return:
+        '''
+        train_date,result_date=self.get_used_date(date)
 
+    def get_used_date(self,date):
+        data=self.pro.trade_cal(start_date=date, end_date=date)
+        if data.values[0,2]==1:
+            train_list=self.get_train_date(date)
+            result_date=self.get_result_date(date)
+            return train_list,result_date
+        else:
+            raise ValueError("日期错误！")
+
+    def get_train_date(self,date,train_list=[]):
+        if train_list.__len__()==7:
+            return train_list
+        d = datetime.datetime.strptime(date, "%Y%m%d")
+        d1 = d - datetime.timedelta(days=1)
+        last_date=d1.date().strftime("%Y%m%d")
+        data = self.pro.trade_cal(start_date=date, end_date=date)
+        if data.values[0, 2] == 1:
+            train_list.append(date)
+            return self.get_train_date(last_date,train_list)
+        else:
+            return self.get_train_date(last_date, train_list)
+
+    def get_result_date(self,date):
+        d = datetime.datetime.strptime(date, "%Y%m%d")
+        d1 = d + datetime.timedelta(days=1)
+        next_date = d1.date().strftime("%Y%m%d")
+        data = self.pro.trade_cal(start_date=next_date, end_date=next_date)
+        if data.values[0, 2] == 1:
+            return next_date
+        else:
+            return self.get_result_date(next_date)
 
 
 if __name__ == '__main__':
 
-    Data_Format().init_data("000005.SZ")
+    # d=datetime.datetime.strptime("20190901","%Y%m%d")
+    # d1=d - datetime.timedelta(days=1)
+    # print(d1.date().strftime("%Y%m%d"))
 
+    # Data_Format().init_data("000005.SZ")
+    # list=Data_Format().get_train_date("20190910")
+    list=Data_Format().get_used_date("20190912")
+    print(list)
+    # d=np.array([1,2,3]).tolist()
+    # print(type(d))
     # np.save(r"C:\File\numpy_data\1.npy", get_data())
     # redis_.hset("data","day1",json.dumps([0,1,2,3]))
     # d=redis_.hget("data","day1")
     # print(d,type(d))
     # print(json.loads(d)[0],type(json.loads(d)))
+
     # for key in redis_.keys("*"):
-    #     print(key,redis_.get(key))
+    #     # redis_.delete(key)
+    #     # print(key ,redis_.type(key))
+    #     if redis_.type(key) == "string":
+    #         print(key, redis_.get(key))
+    #     elif redis_.type(key) == "set":
+    #         print(key, " : ", redis_.scard(key), " : ", redis_.smembers(key))
+    #     elif redis_.type(key) == "list":
+    #         print(key, " : ", redis_.llen(key), " : ")  # , redis_.lrange(key,0,100))
+    #     elif redis_.type(key) == "hash":
+    #         print(key, " : ", redis_.hscan(key))  # , redis_.lrange(key,0,100))
 
